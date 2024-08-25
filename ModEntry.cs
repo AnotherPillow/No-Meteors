a9e4f73812a0609493f2ee5fdf2a454f2ab9ea0e
AnotherPillow <85362273+AnotherPillow@users.noreply.github.com>
2024-08-25 11:50:50 +0100

    Initial Commit 2

---
+++ ModEntry.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using Netcode;
+using StardewModdingAPI;
+using StardewValley.Events;
+
+namespace No_Meteors
+{
+    internal sealed class ModEntry : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            var harmony = new Harmony(this.ModManifest.UniqueID);
+            harmony.Patch(
+               original: AccessTools.Method(typeof(StardewValley.Events.SoundInTheNightEvent), nameof(StardewValley.Events.SoundInTheNightEvent.setUp)),
+               prefix: new HarmonyMethod(typeof(ModEntry), nameof(ModEntry.SoundInTheNightEvent_setUp_Prefix))
+            );
+        }
+
+        internal static bool SoundInTheNightEvent_setUp_Prefix(NetInt ___behavior)
+        {
+            if (___behavior.Value == 1) return false;
+            return true;
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+﻿<font size="6">No Meteors</font>
+
+<br>
+
+A simple mod to prevent meteors from spawning.<font size="5"><br>Download<br></font>
+
+[Nexus Mods](<https://nexusmods.com/stardewvalley/mods/27289>)
